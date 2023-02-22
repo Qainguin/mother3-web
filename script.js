41e630e53fcd466f44d35cb4f0e45721aa13a8cf
Qainguin <94211064+Qainguin@users.noreply.github.com>
2023-02-22 16:57:30 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var NepPlayer = "#emulator";     
+// ID of html element where emulator will be inserted
+ 
+var NepEmu = "gba";         
+// Platform select
+ 
+var NepLang = "en";
+// Option "en,rus,ptBR,ja"
+// Interface language of emulator. EN by default
+ 
+var gameUrl = "Your game url"; // Game Url
